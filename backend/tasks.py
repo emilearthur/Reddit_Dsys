a0e908130c267b2ec79db10f0e0cdb0b6ea4ff07
emilearthur <frederickauthur@hotmail.com>
2021-07-20 18:12:20 +0000

    Done with task

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,4 +1,4 @@
-from celery import Task
+from celery import Celery
 from celery.schedules import crontab
 import logging
 import logging
@@ -10,20 +10,11 @@ import json
 import logging
 import os
 import json
-from celery import Celery
-from fastapi import Depends
 import requests as req
 import json
 
 
-
-from utils.models import Extracted_Created_Date, Subreddit, SubredditDB
-from utils.datetime_fixer import datetimefixer
-
-from app.models.subreddit import SubredditCreate, SubredditInDB
-from app.db.repositories.subreddits import SubredditRepository
-from app.api.dependencies.database import get_repository
-
+from app.models.subreddit import SubredditCreate
 
 logging.basicConfig(filename='redditscrapper.log', encoding='utf-8', level=logging.DEBUG,
                     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
@@ -35,9 +26,7 @@ app = Celery('scrapper_worker')
 app.conf.broker_url = "redis://redis:6379/0"
 app.conf.result_backend = "redis://redis:6379/0"
 app.conf.timezone = 'UTC'
-# new addition 
-# app.conf.task_serializer = 'pickle'
-# app.conf.result_serializer = 'pickle'
+
 
 
 app.conf.beat_schedule = {
@@ -48,40 +37,18 @@ app.conf.beat_schedule = {
 
     'scraping-task-fifteen-min': {
         'task': 'tasks.scrape_data_reddit_bitcoin',
-        'schedule': crontab(minute='*/360')
+        'schedule': crontab(minute='*/20')
     },
-    # # executes daily at midnight
-    # 'scraping-task-midnight-daily': {
-    #     'task': 'tasks.hackernews_rss',
-    #     'schedule': crontab(minute=0, hour=0)
-    # }
+
+    'scraping-task-midnight-daily': {
+        'task': 'tasks.scrape_data_reddit_wallstreet',
+        'schedule': crontab(minute=0, hour=0)
+    }
 }
-
-@app.task
-async def save_data(site: str, data_list = List[dict[str, str]], csv=True) -> None:
-    """[summary]
-
-    Args:
-        site (str): [description]
-        data_list ([type], optional): [description]. Defaults to List[dict[str, str]].
-        csv (bool, optional): [description]. Defaults to True.
-    """
-    outputfilename = f"output_{site}_{datetime.now().strftime('%d-%m-%Y-%H-%M')}"
-    if not csv:
-        with open(os.path.join('./Data', outputfilename+".txt"), "w") as outfile:
-            outfile.write(json.dumps(data_list))
-            outfile.close()
-            logging.info(f"Data saved in Data folder as {outputfilename}.txt")
-    else:
-        data = pd.DataFrame(data_list, columns=['extracted_at', 'id', 'title', 'score', 'url', 'author', 
-                                                'subreddit', 'description', 'created_at'])
-        data.to_csv(os.path.join('./Data', outputfilename+'.csv'), index=False)
-        logging.info(f"Data saved in Data folder as {outputfilename}.csv")
-
 
 
 def save_data(outputfilename: str, data_list = List[dict[str, str]], csv=True) -> None:
-    """[summary]
+    """Function that save scrapped data locally either in .csv format or .txt format. 
 
     Args:
         site (str): [description]
@@ -101,7 +68,7 @@ def save_data(outputfilename: str, data_list = List[dict[str, str]], csv=True) -
 
 @app.task
 def reddit_scrap(outputfile: str, subreddit:str, limit: int = 10, csv: bool = True) -> None:
-    """Extracts subbreddit. Not using asyncpraw library.
+    """Extracts subbreddit, Makes post request to post subreddit endpoint and also saves that scrapped data locally.
 
     Args:
         subreddit (str): [The subreddit you will like to scape]
@@ -129,9 +96,7 @@ def reddit_scrap(outputfile: str, subreddit:str, limit: int = 10, csv: bool = Tr
                 'created_at': post.created_utc,
             }
             data_subreddit = SubredditCreate(**data_dict)
-            # post_subreddit(json.dumps(data_subreddit.dict()))
-            # insert_intodb(new_subreddit=data_subreddit)   
-            url = "http://server:8000/api/subreddit/"
+            url = "http://server:8000/api/subreddit/" 
             req.post(url, data=json.dumps({"new_subreddit": data_subreddit.dict()}))         
             data.append(data_subreddit.dict())
         return save_data(outputfilename=outputfile, data_list=data, csv=csv)
@@ -143,14 +108,21 @@ def reddit_scrap(outputfile: str, subreddit:str, limit: int = 10, csv: bool = Tr
 def scrape_data_reddit_ml(serializer='json'):
     subreddit, csv = 'MachineLearning', True
     outputfile = f"output_{subreddit}_{datetime.now().strftime('%d-%m-%Y-%H-%M')}"
-    reddit_scrap.delay(outputfile = outputfile, subreddit=subreddit, limit=50, csv=True)
-    return "Process Sent"
+    reddit_scrap.delay(outputfile = outputfile, subreddit=subreddit, limit=500, csv=True)
+    return f"Scrapping Subreddit-{subreddit} started"
 
 
 @app.task
 def scrape_data_reddit_bitcoin(serializer='json'):
     subreddit, csv = 'Bitcoin', True
     outputfile = f"output_{subreddit}_{datetime.now().strftime('%d-%m-%Y-%H-%M')}"
-    reddit_scrap.delay(outputfile = outputfile, subreddit=subreddit, limit=50, csv=True)
-    return "Process Sent"
-    
+    reddit_scrap.delay(outputfile = outputfile, subreddit=subreddit, limit=500, csv=True)
+    return f"Scrapping Subreddit-{subreddit} started"
+
+
+@app.task
+def scrape_data_reddit_wallstreet(serializer='json'):
+    subreddit, csv = 'wallstreetbets', True
+    outputfile = f"output_{subreddit}_{datetime.now().strftime('%d-%m-%Y-%H-%M')}"
+    reddit_scrap.delay(outputfile = outputfile, subreddit=subreddit, limit=500, csv=True)
+    return f"Scrapping Subreddit-{subreddit} started"
